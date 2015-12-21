208426c1fa9f7d6d87d0a858c1ccbb02709281b4
Josh <josh.quintus@gmail.com>
2015-12-20 19:32:38 -0500

    Removing/readding the empty project.

--- MasterDevs.TestInsights/MasterDevs.TestInsights/Class1.cs
+++ MasterDevs.TestInsights/MasterDevs.TestInsights/Class1.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.ApplicationInsights;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MasterDevs.TestInsights
 {
     public class Class1
     {
+        public Class1()
+        {
+            TelemetryClient telemetry = new TelemetryClient();
+
+        }
     }
 }
